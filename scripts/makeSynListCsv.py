dc9a0372a90b10adef14d156e0afe9bb1a6ed647
Prajyot Suvarna <prajyotsuvarna@Apples-MacBook-Pro.local>
2020-12-21 19:13:53 +0530

    Deleted old files

--- scripts/makeSynListCsv.py
+++ scripts/makeSynListCsv.py
@@ -1,28 +1,42 @@
-import thesaurusScraper as ts
+import synonymsScraper as ss
 import pandas as pd
 import time
 from pathlib import Path
+import json
 
 # Gets path to the root directory of the repository.
 parentDirPath = Path(__file__).parent.parent
+scraper_obj = ss.Scraper()
+
 
 wordlist_data = pd.read_csv(str(parentDirPath) + '/Data Files/WordsTeachersAverage.csv')
+with open(str(parentDirPath) + '/Data Files/AcronymAbbr.json', 'r') as jsonFile:
+    pos_dict = json.load(jsonFile)
 # Reduces data for levels > A
 difficult_wordlist = wordlist_data[wordlist_data['Teachers Avg'] >= 2]
+
 
 start_time = time.time()
 extracted_els = []
 count = 0
 for index, row in difficult_wordlist.iterrows():
-    syns = ts.retrieve_syns(row.Word)
-    dict1 = {
-         'Word': row.Word,
-         'Synonyms': syns
-    }
-    count += 1
-    extracted_els.append(dict1)
+    syns = scraper_obj.find_syns(row.Word)
+    if len(syns) > 0:
+        try:
+            dict1 = {
+                'Word': row.Word + '_' + pos_dict[row.PoS],
+                'Synonyms': syns
+            }
+        except KeyError:
+            # To handle NaN.
+            dict1 = {
+                'Word': row.Word + '_' + 'na',
+                'Synonyms': syns
+            }
+        count += 1
+        extracted_els.append(dict1)
     print(f'{count} of {len(difficult_wordlist)} completed ', end='\r')
 
 df = pd.DataFrame(extracted_els)
-df.to_csv('WordsSynList.csv', index=False) 
+df.to_csv('WordsSynListNew.csv', index=False) 
 print(f'\n Time taken: {time.time() - start_time}s')
