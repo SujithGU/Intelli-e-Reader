6603b8d5fd30596de28fbcc94a9ed46b32dfa2a8
Sujith G U <sujithgu46@gmail.com>
2020-12-29 13:03:46 +0530

    POS value modifier

--- scripts/synclist_converter.py
+++ scripts/synclist_converter.py
@@ -6,10 +6,11 @@ ndf = pd.read_csv('../Data Files/english_profile_scrape.csv', usecols=cols_to_us
 
 n_dict = {}
 
-pmap = {'determiner': 'DET', 'verb': 'VERB', 'noun': 'NOUN', 'adjective': 'ADJ', 'adverb': 'ADV', 'pronoun': 'PRONOUN',
-        'preposition': 'ADP','nan':'NA','':'NA','None':'NA',
-        'conjunction': 'CONJ', 'exclamation': 'NA',
-        'modal verb': 'NA', 'auxiliary verb': 'NA', 'phrase': 'NA', 'phrasal verb': 'NA'}
+pos_map = {'determiner': 'DET', 'verb': 'VERB', 'noun': 'NOUN', 'adjective': 'ADJ', 'adverb': 'ADV',
+           'pronoun': 'PRONOUN',
+           'preposition': 'ADP', 'nan': 'NA', '': 'NA', 'None': 'NA',
+           'conjunction': 'CONJ', 'exclamation': 'EXC',
+           'modal verb': 'MVB', 'auxiliary verb': 'AVB', 'phrase': 'PHR', 'phrasal verb': 'PVB'}
 # reference map
 cefr_int = {'A1': 1, 'A2': 2, 'B1': 3, 'B2': 4, 'C1': 5, 'C2': 6}
 
@@ -20,7 +21,7 @@ ndf['cefr_avg'] = ndf['cefr'].replace(cefr_int)
 
 ndf['cefr'] = ndf['cefr'].replace(cefr_int).replace(conv_map)
 
-ndf['pos'] = ndf['pos'].replace(pmap)
+ndf['pos'] = ndf['pos'].replace(pos_map)
 
 word_list = []
 pos_lis = []
@@ -30,12 +31,15 @@ cefr_level = []
 for index in ndf.index:
     if '/' not in str(ndf['word'][index]) and len(str(ndf['word'][index]).split(" ")) == 1:
         word_list.append(ndf['word'][index])
-        pos_lis.append(str(ndf['pos'][index]))
+
+        pos_val = str(ndf['pos'][index]).replace("nan", "NA")
+        pos_lis.append(pos_val)
+
         cefr_level.append(ndf['cefr'][index])
         cefr_avg.append(ndf['cefr_avg'][index])
-
+print(str(ndf['pos'][2]))
 # Write to file
 final = {'word': word_list, 'pos': pos_lis, 'cefr_int': cefr_avg, 'cefr': cefr_level}
 fdf = pd.DataFrame(final)
-fdf.to_csv('../Data Files/modified_scrap_data.csv',index=False)
+fdf.to_csv('../Data Files/modified_scrap_data.csv', index=False)
 
