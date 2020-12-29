27da948aa4d690274e7a39cc4383436e4511e9e6
Sujith G U <sujithgu46@gmail.com>
2020-12-29 13:01:55 +0530

    Generation of Master CEFR csv and json

--- scripts/csvToJson.py
+++ scripts/csvToJson.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 path = Path(__file__).parent.parent
 
+
 # Converts 2 column CSV to JSON.
 def convertToJsonFile(relPath, oldFileName, newFileName):
     try:
@@ -16,7 +17,7 @@ def convertToJsonFile(relPath, oldFileName, newFileName):
                 if line_count > 1:
                     rowObj = {
                         # key is the word and value is the synonyms as a list.
-                        row[0]: row[1][1:-1].replace('\'','').split(',')
+                        row[0]: row[1][1:-1].replace('\'', '').split(',')
                     }
                     wordSynDict.update(rowObj)
                 line_count += 1
@@ -32,4 +33,32 @@ def convertToJsonFile(relPath, oldFileName, newFileName):
     except:
         raise Exception('File does not exist')
 
-convertToJsonFile('/Data Files/', oldFileName='SynListSynonyms.csv',newFileName='SynonymsDict.json')
+
+# Converts 4 column CSV to JSON. - word - pos - cefr_int - cefr
+def convert_to_json(relPath, oldFileName, newFileName):
+    try:
+        word_pos_dict = {}
+        with open(str(path) + relPath + oldFileName) as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+
+            for rows in csv_reader:
+                key = rows['word']
+                word_pos_dict[key] = rows
+
+        try:
+            with open(str(path) + relPath + newFileName, 'w') as outfile:
+                json.dump(word_pos_dict, outfile)
+            print('Success: File created')
+        except:
+            print('Error in creating file')
+
+    except:
+        raise Exception('File does not exist')
+
+
+'''
+Un-comment the following line to generate cefr json
+'''
+# convert_to_json('/Data Files/', oldFileName='master_cefr.csv',newFileName='master_cefr.json')
+
+convertToJsonFile('/Data Files/', oldFileName='.csv', newFileName='SynonymsDict.json')
