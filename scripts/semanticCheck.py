6a2046994a9797356a932d8a305e806fb016523f
Sujith G U <sujithgu46@gmail.com>
2021-01-04 15:09:28 +0530

    Code for Reading PDF and Operating on it

--- scripts/semanticCheck.py
+++ scripts/semanticCheck.py
@@ -4,11 +4,14 @@ from sentence_transformers import SentenceTransformer
 from nltk.tag import pos_tag
 
 class SemanticCheck:
+    # NLTK tag list -> custom tag for project
+    pos_convert = {'NN': 'n', 'NNS': 'n', 'VB': 'v', 'VBG': 'v', 'VBD': 'v',
+                   'VBN': 'v', 'VBP': 'v', 'VBZ': 'v', 'JJ': 'aj', 'JJR': 'aj', 'JJS': 'aj',
+                   'RB': 'av', 'RBR': 'av', 'RBS': 'av', 'IN': 'pp', 'CC': 'cj'}
 
     def __init__(self):
         self.model = SentenceTransformer('bert-base-nli-mean-tokens')
-        
-    
+
     def form_sentences(self, query, word, synonyms, index, seperator):
         """This method makes sentences for all the synonyms of the word provided.
 
@@ -22,9 +25,9 @@ class SemanticCheck:
         Returns:
             (string, [string]): A string of the merged sentences, list of individual sentences
         """
-        words = query.split(' ')   
+        words = query.split(' ')
         sentences = list()
-        
+
         for synonym in synonyms:
             word_list = copy.deepcopy(words)
             word_list[index] = synonym
@@ -32,13 +35,12 @@ class SemanticCheck:
             for word in word_list:
                 sentence = sentence + ' ' + word
             sentences.append(sentence.strip())
-    
-        # seperator = '\n'
+
+        # separator = '\n'
         merged_sentences = seperator.join(sentences)
         return merged_sentences, sentences
-    
-    
-    def checkSynPos(self, query, word, synonyms, index):
+
+    def checkSynPos(self, query, word, synonyms, index, req_pos):
         """Returns syns with same PoS as the original word.
 
         Args:
@@ -46,6 +48,7 @@ class SemanticCheck:
             word (string): Word to be replaced.
             synonyms ([string]): List of synonyms of the word to be replaced.
             index (int): Position of the word in the query.
+            :param req_pos: part of speech we are looking for
 
         Returns:
             [string]: List of acceptable synonyms.
@@ -55,22 +58,22 @@ class SemanticCheck:
         merged_sentences, sentences = self.form_sentences(query, word, wordList, index, '\n')
         del sentences
         tagged_sent = pos_tag(merged_sentences.split())
-        
-        acceptable_syns = list()
-        count = 0
+
+        acceptable_syns = set()
+        # count = 0
         for tag in tagged_sent:
-            if tag[0] == word and index == count:
-                req_pos = tag[1]
-                count += 1
-                continue
-            if tag[0] in synonyms and tag[1] == req_pos:
-                acceptable_syns.append(tag[0])
-            count += 1
+            # TODO : Please check if this is OK!
+            # if tag[0] == word and index == count:
+            #     req_pos = tag[1]
+            #     count += 1
+            #     continue
+            if tag[0] in synonyms and self.pos_convert.get(tag[1]) == req_pos:
+                acceptable_syns.add(tag[0])
+            # count += 1
         return acceptable_syns
-        
-        
+
     def calcSemanticScore(self, query, word, synonyms, index):
-        """Caclulates the semantic similarity for the sentences with syns substituted.
+        """Calculates the semantic similarity for the sentences with syns substituted.
 
         Args:
             query (string): The original sentence
@@ -81,26 +84,21 @@ class SemanticCheck:
         Returns:
             [tuple]: Returns a list of tuples of (word, similarity, sentence)
         """
-        
+
         merged_sentences, sentences = self.form_sentences(query, word, synonyms, index, '\n')
-        corpus=[i for i in merged_sentences.split('\n')if i != ''and len(i.split(' ')) >= 4]
+        corpus = [i for i in merged_sentences.split('\n') if i != '' and len(i.split(' ')) >= 4]
         corpus_embeddings = self.model.encode(corpus)
-        
+
         queries = [query]
         query_embeddings = self.model.encode(queries)
-        
+
         closest_n = 5
-        
+
         # Compute distances for the sentences.
         for query, query_embedding in zip(queries, query_embeddings):
             distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
-            
+
             syn_distances = zip(synonyms, 1 - distances, sentences)
             syn_distances = sorted(syn_distances, key=lambda x: x[1], reverse=True)
-        
+
         return syn_distances[:closest_n]
-
-
-        
-        
-        
